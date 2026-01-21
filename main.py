20644017f66906553a14e31b96be408f36f2ca92
gromit <fredrikudengen@gmail.com>
2026-01-21 12:09:00 +0100

    minor changes

--- main.py
+++ main.py
@@ -21,9 +21,7 @@ room_manager = RoomManager(world, player, camera)
 run = True
 while run:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             run = False
 
     # update
